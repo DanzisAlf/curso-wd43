7a1d6c2720fa41cd043c80529d019e09f368e028
DanzisAlf <migalha_fei@hotmail.com>
2017-05-20 15:54:41 -0300

    7,7

---
+++ produto.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	  <title> Mirror Fashion</title>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width"> <!--mobile-->
+	  <link rel="stylesheet" href="css/reset.css">
+	  <link rel="stylesheet" href="css/estilos.css">
+	  <link rel="stylesheet" href="css/mobile.css" media="(max-width:939px)">
+	</head>
+	  <body>
+	    <?php include("cabecalho.php");?>
+	    <?php include("rodape.php");?>
+	  </body>
+</html>
---
+++ rodape.php
@@ -0,0 +1,12 @@
+	<footer>
+		<div class="container">
+			<img src="img/logo-rodape.png" alt="Logo mirror fashion">
+
+			<ul class="social">
+				<li><a href="http://www.facebook.com">Facebook</a></li>
+				<li><a href="http://www.twitter.com">Twitter</a></li>
+				<li><a href="http://www.plus.google.com">GPlus</a></li>
+			</ul>
+		</div>
+	</footer>
+
