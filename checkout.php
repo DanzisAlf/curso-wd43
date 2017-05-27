c9d6d72cab92fe4e53a38c3bd5a8062d4662dc6e
DanzisAlf <migalha_fei@hotmail.com>
2017-05-27 15:43:26 -0300

    exercicio 10,4

--- checkout.php
+++ checkout.php
@@ -4,30 +4,40 @@
   <meta charset="utf-8">
   <title>Checkout MF</title>
   <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 
 <body>
-  <h1>Ótima Escolha</h1>
-  <p>Obrigado por escolher a MF</p>
-  
-  <!-- montar lista de definicoes, outro tipo alem de ol e ul -->
-  <h2>Sua Compra</h2>
-  
-  <dl>
-    <dt>Cor</dt> <!-- definition term -->
-    <dd><?= $_POST['cor'] ?></dd>
-    
-    <dt>Produto</dt>
-    <dd><?= $_POST['nome'] ?></dd> 
-    
-    <dt>Tamanho</dt>
-    <dd><?= $_POST['tamanho'] ?></dt>
-    
-    <dt>Preço</dt>
-    <dd><?= $_POST['preco'] ?></dt>
-  </dl>
-  
-    
+  <div class="jumbotron"> <!-- bootstrap -->
+    <div class="container"> <!-- bootstrap -->
+      <h1>Ótima Escolha</h1>
+      <p>Obrigado por escolher a MF</p>
+    </div>
+  </div>
+  <div class="container"> <!-- centralizar -->
+    <!-- montar lista de definicoes, outro tipo alem de ol e ul -->
+    <div class="panel panel-success"> <!-- bootstrap panel-default. opcional panel.success -->
+      <div class="panel-heading"> <!-- bootstrap -->
+        <h2 class="panel-title">Sua Compra</h2>
+      </div>
+      <div class="panel-body">
+        <img src="img/produtos/foto1-<?= $_POST['cor'] ?>.png" class="img-thumbnail img-responsive"> <!-- bootstrap e escolher a cor com php -->
+        <dl>
+          <dt>Cor</dt> <!-- definition term -->
+          <dd><?= $_POST['cor'] ?></dd>
+          
+          <dt>Produto</dt>
+          <dd><?= $_POST['nome'] ?></dd> 
+          
+          <dt>Tamanho</dt>
+          <dd><?= $_POST['tamanho'] ?></dt>
+          
+          <dt>Preço</dt>
+          <dd><?= $_POST['preco'] ?></dt>
+        </dl>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
 
