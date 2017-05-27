b49253bd4b452a44ae82f3e3bfdb21243bba4b2e
DanzisAlf <migalha_fei@hotmail.com>
2017-05-27 17:08:11 -0300

    exercicio 10.10

--- checkout.php
+++ checkout.php
@@ -37,6 +37,64 @@
         </dl>
       </div>
     </div>
+    <form>
+      <fieldset> <!-- cria agrupador. para forms, usa-se fieldset -->
+        <legend>Dados pessoais</legend>
+        
+        <div class="form-group"> <!-- bootstrap. formatar grupo-->
+          <label for="nome">Nome completo</label> <!-- o campo nome completo recebe FOR nome. todo label é seguido de um input-->
+          <input type="text" class="form-control" id="nome" name="nome" autofocus>   
+        </div>  
+      
+        <div class="form-group"> <!-- bootstrap. formatar grupo-->
+          <label for="email">em@il</label> <!-- o campo email recebe FOR email. todo label é seguido de um input-->
+          
+          <div class="input-group">
+            <span class="input-group-addon">@</span>
+            <input type="email" class="form-control" id="email" name="email" placeholder="me@myself.com">
+          </div>
+        </div>
+
+        <div class="form-group"> <!-- bootstrap. formatar grupo-->
+          <label for="cpf">CPF</label> <!-- o campo cpf recebe FOR cpf. todo label é seguido de um input-->
+          <input type="text" class="form-control" id="cpf" name="cpf">
+        </div>
+
+<!-- para checkbox, o bootstrap funciona diferente do anterior -->
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="sim" name="spam" checked>
+            Quero receber spam
+          </label>
+        </div>
+            
+      </fieldset>
+
+      <fieldset>
+        <legend>Cartão de crédito</legend>
+        <div class="form-group">
+          <label for="numero-cartao">Número-cvv</label>
+          <input type="text" class="form-control" id="numero-cartao" name="nome-cartao" placeholder="0000.0000.0000.0000-000">
+        </div>
+        
+        <!-- incluir um dropdown usando select no lugar de type-->
+        <div class="form-group">
+          <label for="bandeira-cartao">Bandeira</label>
+          <select name="bandeira-cartao" class="form-control" id="bandeira=cartao" name="bandeira-cartao">
+            <option value="master">MasterCard</option>
+            <option value="visa">Visa</option>
+            <option value="amex">American Express</option>
+          </select>
+        </div>
+      </fieldset>
+      
+      <button type="submit" class="btn-primary"> <!-- classe bootstrap para botoes-->
+        <span class="glyphicon glyphicon-thumbs-up btn-lg pull-right"></span>
+        Confirmar Pedido
+      </button>
+    </form> 
+      
+      
   </div>
 </body>
 </html>
