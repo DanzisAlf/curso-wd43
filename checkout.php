d03d753113ccf646f68560a65e439384505da730
DanzisAlf <migalha_fei@hotmail.com>
2017-06-03 10:21:15 -0300

    exercicio 10.14

--- checkout.php
+++ checkout.php
@@ -16,86 +16,90 @@
     </div>
   </div>
   <div class="container"> <!-- centralizar -->
-    <!-- montar lista de definicoes, outro tipo alem de ol e ul -->
-    <div class="panel panel-success"> <!-- bootstrap panel-default. opcional panel.success -->
-      <div class="panel-heading"> <!-- bootstrap -->
-        <h2 class="panel-title">Sua Compra</h2>
-      </div>
-      <div class="panel-body">
-        <img src="img/produtos/foto1-<?= $_POST['cor'] ?>.png" class="img-thumbnail img-responsive"> <!-- bootstrap e escolher a cor com php -->
-        <dl>
-          <dt>Cor</dt> <!-- definition term -->
-          <dd><?= $_POST['cor'] ?></dd>
-          
-          <dt>Produto</dt>
-          <dd><?= $_POST['nome'] ?></dd> 
-          
-          <dt>Tamanho</dt>
-          <dd><?= $_POST['tamanho'] ?></dt>
-          
-          <dt>Preço</dt>
-          <dd><?= $_POST['preco'] ?></dt>
-        </dl>
-      </div>
-    </div>
-    <form>
-      <fieldset> <!-- cria agrupador. para forms, usa-se fieldset -->
-        <legend>Dados pessoais</legend>
-        
-        <div class="form-group"> <!-- bootstrap. formatar grupo-->
-          <label for="nome">Nome completo</label> <!-- o campo nome completo recebe FOR nome. todo label é seguido de um input-->
-          <input type="text" class="form-control" id="nome" name="nome" autofocus required>   
-        </div>  
-      
-        <div class="form-group"> <!-- bootstrap. formatar grupo-->
-          <label for="email">em@il</label> <!-- o campo email recebe FOR email. todo label é seguido de um input-->
-          
-          <div class="input-group">
-            <span class="input-group-addon">@</span>
-            <input type="email" class="form-control" id="email" name="email" placeholder="me@myself.com" required>
+    <div class="row"> <!-- bootstrap, para utilizar o col-xx. Ele divide a div em 12 colunas-->
+      <!-- montar lista de definicoes, outro tipo alem de ol e ul -->
+      <div class="col-sm-4"> <!-- no row, utiliza 4 dos 12 espaços, para telas small-->
+        <div class="panel panel-success"> <!-- bootstrap panel-default. opcional panel.success -->
+          <div class="panel-heading"> <!-- bootstrap -->
+            <h2 class="panel-title">Sua Compra</h2>
+          </div>
+          <div class="panel-body">
+            <img src="img/produtos/foto1-<?= $_POST['cor'] ?>.png" class="img-thumbnail img-responsive"> <!-- bootstrap e escolher a cor com php -->
+            <dl>
+              <dt>Cor</dt> <!-- definition term -->
+              <dd><?= $_POST['cor'] ?></dd>
+              
+              <dt>Produto</dt>
+              <dd><?= $_POST['nome'] ?></dd> 
+              
+              <dt>Tamanho</dt>
+              <dd><?= $_POST['tamanho'] ?></dt>
+              
+              <dt>Preço</dt>
+              <dd><?= $_POST['preco'] ?></dt>
+            </dl>
           </div>
         </div>
-
-        <div class="form-group"> <!-- bootstrap. formatar grupo-->
-          <label for="cpf">CPF</label> <!-- o campo cpf recebe FOR cpf. todo label é seguido de um input-->
-          <input type="text" class="form-control" id="cpf" name="cpf">
-        </div>
-
-<!-- para checkbox, o bootstrap funciona diferente do anterior -->
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="sim" name="spam" checked>
-            Quero receber spam
-          </label>
-        </div>
+      </div>
+      <form class="col-sm-8"> <!-- utiliza o restante dos 2/3 das colunas-->
+        <div class="row"> <!-- faz outra divisao por 12 colunas-->
+          <fieldset class="col-md-6"> <!-- cria agrupador. para forms, usa-se fieldset. ajustando classe bootstrap para metade do row-->
+            <legend>Dados pessoais</legend>
             
-      </fieldset>
+            <div class="form-group"> <!-- bootstrap. formatar grupo-->
+              <label for="nome">Nome completo</label> <!-- o campo nome completo recebe FOR nome. todo label é seguido de um input-->
+              <input type="text" class="form-control" id="nome" name="nome" autofocus required> <!--required para obrigatorio"   
+            </div>  
+          
+            <div class="form-group"> <!-- bootstrap. formatar grupo-->
+              <label for="email">em@il</label> <!-- o campo email recebe FOR email. todo label é seguido de um input-->
+              
+              <div class="input-group">
+                <span class="input-group-addon">@</span>
+                <input type="email" class="form-control" id="email" name="email" placeholder="me@myself.com" required>
+              </div>
+            </div>
 
-      <fieldset>
-        <legend>Cartão de crédito</legend>
-        <div class="form-group">
-          <label for="numero-cartao">Número-cvv</label>
-          <input type="text" class="form-control" id="numero-cartao" name="nome-cartao" placeholder="0000.0000.0000.0000-000">
+            <div class="form-group"> <!-- bootstrap. formatar grupo-->
+              <label for="cpf">CPF</label> <!-- o campo cpf recebe FOR cpf. todo label é seguido de um input-->
+              <input type="text" class="form-control" id="cpf" name="cpf">
+            </div>
+
+    <!-- para checkbox, o bootstrap funciona diferente do anterior -->
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" value="sim" name="spam" checked>
+                Quero receber spam
+              </label>
+            </div>
+                
+          </fieldset>
+
+          <fieldset class="col-md-6"> <!-- outra metade do row acima do fieldset-->
+            <legend>Cartão de crédito</legend>
+            <div class="form-group">
+              <label for="numero-cartao">Número-cvv</label>
+              <input type="text" class="form-control" id="numero-cartao" name="nome-cartao" placeholder="0000.0000.0000.0000-000">
+            </div>
+            
+            <!-- incluir um dropdown usando select no lugar de type-->
+            <div class="form-group">
+              <label for="bandeira-cartao">Bandeira</label>
+              <select name="bandeira-cartao" class="form-control" id="bandeira=cartao" name="bandeira-cartao">
+                <option value="master">MasterCard</option>
+                <option value="visa">Visa</option>
+                <option value="amex">American Express</option>
+              </select>
+            </div>
+          </fieldset>
         </div>
+          <button type="submit" class="btn btn-primary btn-lg pull-right"> <!-- classe bootstrap para botoes-->
+            <span class="glyphicon glyphicon-thumbs-up"></span>
+            Confirmar Pedido
+          </button>
+      </form> 
         
-        <!-- incluir um dropdown usando select no lugar de type-->
-        <div class="form-group">
-          <label for="bandeira-cartao">Bandeira</label>
-          <select name="bandeira-cartao" class="form-control" id="bandeira=cartao" name="bandeira-cartao">
-            <option value="master">MasterCard</option>
-            <option value="visa">Visa</option>
-            <option value="amex">American Express</option>
-          </select>
-        </div>
-      </fieldset>
-      
-      <button type="submit" class="btn-primary"> <!-- classe bootstrap para botoes-->
-        <span class="glyphicon glyphicon-thumbs-up btn-lg pull-right"></span>
-        Confirmar Pedido
-      </button>
-    </form> 
-      
-      
+    </div>    
   </div>
 </body>
 </html>
